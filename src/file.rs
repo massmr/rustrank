c7cdeea6da45eda622720ad3076e372db8a48a40
massmr <marcellin.massimo@gmail.com>
2025-01-17 13:36:17 +0100

    feat : add of file.rs in order to read csv file, then write its content in a Graph

---
+++ src/file.rs
@@ -0,0 +1,8 @@
+// I want to read a csv file from ../assets/ 
+// It has two columns inside. 
+// and write a function that will return a VecGraph of Vec<i32> or
+// MatrixGraph Vec<Vec<i32>> from this file :
+
+// csv file example :
+// 1st column : emmiting adress
+// 2nd column : receiving adress
