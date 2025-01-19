c76db319234aad69af413f2cd700c1b8c4b9a710
massmr <marcellin.massimo@gmail.com>
2025-01-20 00:45:36 +0100

    docs : document the csv logic

--- src/file.rs
+++ src/file.rs
@@ -3,33 +3,41 @@ use std::collections::HashMap;
 use csv::ReaderBuilder;
 
 pub fn read_csv(file_path: &str) -> Result<Vec<Vec<i32>>, Box<dyn Error>> {
+    // read the CSV file, delimiter is ';'
+    // Initialize the matrix
+    // Initialize nodes HashMap
+    // Initialize n as node count
     let mut rdr = ReaderBuilder::new().delimiter(b';').from_path(file_path)?;
     let mut matrix = Vec::new();
     let mut nodes = HashMap::new();
-    let mut node_count = 0;
+    let mut n = 0;
 
-    // Première passe pour compter les nœuds et créer le mapping
+    // First pass to count the number of nodes
+    // Iterate over each row of the CSV file
     for result in rdr.records() {
         let record = result?;
         let input_address = record.get(0).unwrap().to_string();
         let output_address = record.get(1).unwrap().to_string();
 
+        // If the node is not in the HashMap, add it
         if !nodes.contains_key(&input_address) {
-            nodes.insert(input_address.clone(), node_count);
+            nodes.insert(input_address.clone(), n);
             node_count += 1;
         }
+        // If the node is not in the HashMap, add it
         if !nodes.contains_key(&output_address) {
-            nodes.insert(output_address.clone(), node_count);
+            nodes.insert(output_address.clone(), n);
             node_count += 1;
         }
     }
 
-    matrix.resize(node_count, vec![0; node_count]);
+    // Initialize the matrix with zeros
+    matrix.resize(node_count, vec![0; n]);
 
-    // Réinitialiser le lecteur CSV pour la deuxième passe
+    // Reset the reader
     let mut rdr = ReaderBuilder::new().delimiter(b';').from_path(file_path)?;
 
-    // Deuxième passe pour remplir la matrice d'adjacence
+    // Second pass to fill the matrix
     for result in rdr.records() {
         let record = result?;
         let input_address = record.get(0).unwrap().to_string();
