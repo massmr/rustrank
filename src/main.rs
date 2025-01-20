151524dfa0513d9632ce095bddaabcd0bf1676d3
massmr <marcellin.massimo@gmail.com>
2025-01-20 08:27:04 +0100

    docs : add english comments

--- src/main.rs
+++ src/main.rs
@@ -11,16 +11,14 @@ use crate::file::read_csv;
 use std::error::Error;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    // Lire le fichier CSV et créer une matrice d'adjacence
-    let file_path = "assets/data/sample1.csv";
+    // Read csv file
+    let file_path = "assets/data/fullsample.csv";
     let matrix = read_csv(file_path)?;
 
-    // Créer un graphe à partir de la matrice d'adjacence
+    // Create matrix graph from csv file
     let matrix_graph = MatrixGraph::new(matrix);
-    println!("Matrix graph :");
-    matrix_graph.display();
     
-    // Calculer le PageRank pour le graphe
+    // Apply PageRank algorithm
     let matrix_rank = MatrixRank::new(matrix_graph, 0.85, 100, 0.0001);
     matrix_rank.page_rank();
 
