1aa0f1b07125c10dc6ad6e69e12154c95f58858f
massmr <marcellin.massimo@gmail.com>
2025-01-21 11:23:32 +0100

    feat : add node_names

--- src/main.rs
+++ src/main.rs
@@ -12,14 +12,14 @@ use std::error::Error;
 
 fn main() -> Result<(), Box<dyn Error>> {
     // Read csv file
-    let file_path = "assets/data/fullsample.csv";
-    let matrix = read_csv(file_path)?;
+    let file_path = "./assets/data/easysample.csv";
+    let (matrix, node_names) = read_csv(file_path)?;
 
     // Create matrix graph from csv file
     let matrix_graph = MatrixGraph::new(matrix);
     
     // Apply PageRank algorithm
-    let matrix_rank = MatrixRank::new(matrix_graph, 0.85, 100, 0.0001);
+    let matrix_rank = MatrixRank::new(matrix_graph, node_names, 0.85, 100, 0.0001);
     matrix_rank.page_rank();
 
     Ok(())
