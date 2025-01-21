8a1e20c5eba088f3f611e2906d2ab0a089da55b1
massmr <marcellin.massimo@gmail.com>
2025-01-21 11:43:27 +0100

    feat : add command line arguments for input files

--- src/main.rs
+++ src/main.rs
@@ -5,19 +5,23 @@
 mod graph;
 mod pagerank;
 mod file;
+use std::env;
 use crate::graph::MatrixGraph;
 use crate::pagerank::MatrixRank;
 use crate::file::read_csv;
 use std::error::Error;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    // Read csv file
-    let file_path = "./assets/data/easysample.csv";
+
+    let args : Vec<String> = env::args().collect();
+    let file_path = &args[1] || "./assets/data/easysample.csv";
+    
+    // Create matrix and node names from csv file
     let (matrix, node_names) = read_csv(file_path)?;
 
-    // Create matrix graph from csv file
+    // Create MatrixGraph from matrix
     let matrix_graph = MatrixGraph::new(matrix);
-    
+   
     // Apply PageRank algorithm
     let matrix_rank = MatrixRank::new(matrix_graph, node_names, 0.85, 100, 0.0001);
     matrix_rank.page_rank();
